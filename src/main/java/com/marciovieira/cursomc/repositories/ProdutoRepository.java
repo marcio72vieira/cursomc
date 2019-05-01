b144bada69f80767b9955ee30b5691653d439c89
marciovieira <marcio72vieira@hotmail.com>
2019-05-01 00:21:10 -0300

    Produtos e associação muitos para muitos

---
+++ src/main/java/com/marciovieira/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.marciovieira.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.marciovieira.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
